a58f0072b6fffaa535164999e174c1f317d2e352
benjo247 <benjaminlinge@googlemail.com>
2026-05-15 12:44:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *   - External images: cache as encountered, served from cache when possible
  */
 
-var VERSION = 'ap-v19';
+var VERSION = 'ap-v20';
 var SHELL_CACHE = 'kp-shell-' + VERSION;
 var DATA_CACHE = 'kp-data-' + VERSION;
 var IMG_CACHE = 'kp-img-' + VERSION;
